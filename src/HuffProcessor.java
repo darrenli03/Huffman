5dd7883e3fe359a7d636fc853b51735b786b1c2d
darrenli03 <71399017+darrenli03@users.noreply.github.com>
2023-11-27 23:27:34 -0500

    implemented decompress and readTree methods + formatting

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -4,94 +4,134 @@ import java.util.PriorityQueue;
  * Although this class has a history of several years,
  * it is starting from a blank-slate, new and clean implementation
  * as of Fall 2018.
- * <P>
+ * <p>
  * Changes include relying solely on a tree for header information
  * and including debug and bits read/written information
- * 
- * @author Owen Astrachan
  *
+ * @author Owen Astrachan
+ * @author Darren Li
+ * <p>
  * Revise
  */
 
 public class HuffProcessor {
 
-	private class HuffNode implements Comparable<HuffNode> {
-		HuffNode left;
-		HuffNode right;
-		int value;
-		int weight;
+    private class HuffNode implements Comparable<HuffNode> {
+        HuffNode left;
+        HuffNode right;
+        int value;
+        int weight;
 
-		public HuffNode(int val, int count) {
-			value = val;
-			weight = count;
-		}
-		public HuffNode(int val, int count, HuffNode ltree, HuffNode rtree) {
-			value = val;
-			weight = count;
-			left = ltree;
-			right = rtree;
-		}
+        public HuffNode(int val, int count) {
+            value = val;
+            weight = count;
+        }
 
-		public int compareTo(HuffNode o) {
-			return weight - o.weight;
-		}
-	}
+        public HuffNode(int val, int count, HuffNode ltree, HuffNode rtree) {
+            value = val;
+            weight = count;
+            left = ltree;
+            right = rtree;
+        }
 
-	public static final int BITS_PER_WORD = 8;
-	public static final int BITS_PER_INT = 32;
-	public static final int ALPH_SIZE = (1 << BITS_PER_WORD); 
-	public static final int PSEUDO_EOF = ALPH_SIZE;
-	public static final int HUFF_NUMBER = 0xface8200;
-	public static final int HUFF_TREE  = HUFF_NUMBER | 1;
+        public int compareTo(HuffNode o) {
+            return weight - o.weight;
+        }
+    }
 
-	private boolean myDebugging = false;
-	
-	public HuffProcessor() {
-		this(false);
-	}
-	
-	public HuffProcessor(boolean debug) {
-		myDebugging = debug;
-	}
+    public static final int BITS_PER_WORD = 8;
+    public static final int BITS_PER_INT = 32;
+    public static final int ALPH_SIZE = (1 << BITS_PER_WORD);
+    public static final int PSEUDO_EOF = ALPH_SIZE;
+    public static final int HUFF_NUMBER = 0xface8200;
+    public static final int HUFF_TREE = HUFF_NUMBER | 1;
 
-	/**
-	 * Compresses a file. Process must be reversible and loss-less.
-	 *
-	 * @param in
-	 *            Buffered bit stream of the file to be compressed.
-	 * @param out
-	 *            Buffered bit stream writing to the output file.
-	 */
-	public void compress(BitInputStream in, BitOutputStream out){
+    private boolean myDebugging = false;
+
+    public HuffProcessor() {
+        this(false);
+    }
+
+    public HuffProcessor(boolean debug) {
+        myDebugging = debug;
+    }
+
+    /**
+     * Compresses a file. Process must be reversible and loss-less.
+     *
+     * @param in  Buffered bit stream of the file to be compressed.
+     * @param out Buffered bit stream writing to the output file.
+     */
+    public void compress(BitInputStream in, BitOutputStream out) {
 
 
-		// remove all this code when implementing compress
+        // remove all this code when implementing compress
+        while (true) {
+            int val = in.readBits(BITS_PER_WORD);
+            if (val == -1) break;
+            out.writeBits(BITS_PER_WORD, val);
+        }
+        out.close();
+    }
+
+    /**
+     * Decompresses a file. Output file must be identical bit-by-bit to the
+     * original.
+     *
+     * @param in  Buffered bit stream of the file to be decompressed.
+     * @param out Buffered bit stream writing to the output file.
+     */
+    public void decompress(BitInputStream in, BitOutputStream out) {
+
+        int bits = in.readBits(BITS_PER_INT);
+        if (bits != HUFF_TREE) throw new HuffException("invalid magic number " + bits);
+
+        HuffNode root = readTree(in);
+        HuffNode current = root;
+        while(true){
+            int nextBit = in.readBits(1);
+            if(nextBit == -1) throw new HuffException(("Bad input, no PSEUDO_EOF"));
+
+            if(nextBit == 1){
+                current = current.right;
+            } else if(nextBit == 0) {
+                current = current.left;
+            }
+
+            if(current.left == null && current.right == null){
+                if(current.value == PSEUDO_EOF) break;
+                else {
+                    out.writeBits(BITS_PER_WORD, current.value);
+                    current = root;
+                }
+            }
+
+        }
+
+
+        // remove all code when implementing decompress
+/*
 		while (true){
 			int val = in.readBits(BITS_PER_WORD);
 			if (val == -1) break;
 			out.writeBits(BITS_PER_WORD, val);
 		}
 		out.close();
-	}
 
-	/**
-	 * Decompresses a file. Output file must be identical bit-by-bit to the
-	 * original.
-	 *
-	 * @param in
-	 *            Buffered bit stream of the file to be decompressed.
-	 * @param out
-	 *            Buffered bit stream writing to the output file.
-	 */
-	public void decompress(BitInputStream in, BitOutputStream out){
+ */
+    }
 
-		// remove all code when implementing decompress
+    private HuffNode readTree(BitInputStream in) {
+        int bit = in.readBits(1);
+        if (bit == -1) throw new HuffException("invalid magic number " + bit);
+        if (bit == 0) {
+            HuffNode left = readTree(in);
+            HuffNode right = readTree(in);
+            return new HuffNode(0, 0, left, right);
+        } else {
+            var value = in.readBits(BITS_PER_WORD + 1);
+            return new HuffNode(value, 0, null, null);
+        }
+    }
 
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
-		out.close();
-	}
 }
